5ab06895bfcbed76bc48d1255250dd15903c8a7c
André Luís Menegatti <>
2019-11-12 18:02:00 -0300

    Update build_dd_df.R

--- R/build_dd_df.R
+++ R/build_dd_df.R
@@ -3,14 +3,19 @@ build_dd_df <- function(df) {
     mutate(treat1 = if_else(comprasnet == 1 & abertura_lances >= data_20s & abertura_lances < data_3s, 1, 0),
            treat2 = if_else(comprasnet == 1 & abertura_lances > data_3s, 1, 0),
            bimestre = factor(inicio_bimestre), mes = factor(inicio_mes), semana = factor(inicio_semana),
+           municipio = as.factor(municipio),
            unidade_compradora = as.factor(str_c('unidade_', unidade_compradora)),
            marca_vencedor = as.factor(str_c('marca_', marca_vencedor_principais)),
-           indice_mes = dense_rank(inicio_mes)
+           indice_mes = dense_rank(inicio_mes) - 1,
+           group_trend = comprasnet*indice_mes
     ) %>%
     mutate(regime_juridico = case_when(abertura_lances < data_20s ~ 1,
                                        abertura_lances >= data_20s & abertura_lances < data_3s ~ 2,
-                                       abertura_lances >= data_3s ~ 3)
+                                       abertura_lances >= data_3s ~ 3) %>% as.factor()
     ) %>%
     group_by(regime_juridico) %>%
-    mutate(indice_mes_por_regime = dense_rank(inicio_mes))
+    mutate(indice_mes_por_regime = dense_rank(inicio_mes) - 1) %>%
+    ungroup() %>%
+    mutate(treat_trend1 = treat1 * indice_mes_por_regime,
+           treat_trend2 = treat2 * indice_mes_por_regime)
 }
